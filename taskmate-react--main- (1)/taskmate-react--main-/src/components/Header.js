596a4f7eb2cb85584187606547dceeca6cfffedf
Srashti Gupta <84034944+SrashtiGupta498@users.noreply.github.com>
2025-07-19 21:39:26 +0530

    Update Header.js

--- taskmate-react--main- (1)/taskmate-react--main-/src/components/Header.js
+++ taskmate-react--main- (1)/taskmate-react--main-/src/components/Header.js
@@ -13,7 +13,7 @@ const Header = ({children, theme, setTheme}) => {
                 <span onClick={() => setTheme("dark")} className={ theme === "dark" ? "dark activeTheme" : "dark"}></span>
                 <span onClick={() => setTheme("gOne")} className={ theme === "gOne" ? "gOne activeTheme" : "gOne"}></span>
                 <span onClick={() => setTheme("gTwo")} className={ theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}></span>
-                <span onClick={() => setTheme("gThree")} className={ theme === "gThree" ? "gThree activeTheme" : "gThree"}></span>
+            
             </span>
         </header>
     )
